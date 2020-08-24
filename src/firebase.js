7b056bedf1f421d082b43ceca01b83480f57a586
jaypatel0717 <jaypatel0717@gmail.com>
2020-08-24 19:09:04 -0400

    add without code

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD7OIXiN8PzfiDGl5czaiLbgx0KDlToFPo",
+    authDomain: "clone-c98d3.firebaseapp.com",
+    databaseURL: "https://clone-c98d3.firebaseio.com",
+    projectId: "clone-c98d3",
+    storageBucket: "clone-c98d3.appspot.com",
+    messagingSenderId: "924635122026",
+    appId: "1:924635122026:web:b06eb58cd3a055d7eb3d13",
+    measurementId: "G-83SGBLRWCH"
+  };
