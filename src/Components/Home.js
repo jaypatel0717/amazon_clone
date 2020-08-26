023e7256946fae03b0ad1b486c305d24e3220eb7
jaypatel0717 <jaypatel0717@gmail.com>
2020-08-25 22:36:27 -0400

    add product component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../Css/Home.css';
+import Product from './Product';
 
 
 function Home() {
@@ -8,6 +9,13 @@ function Home() {
             <img className="home_image"
                 src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ZmI5MDQwZjEt/ZmI5MDQwZjEt-ZmMxNGZiOTQt-w3000._CB429000162_.jpg" 
                 alt=""
+            />
+            <Product 
+                id="1234"
+                title="Canon USA Ivy CLIQ Mint Green, 3884C001"
+                price={59.99}
+                rating={4}
+                image="https://images-na.ssl-images-amazon.com/images/I/71N4YC3039L._AC_SX679_.jpg"
             />
         </div>
     )
---
+++ src/Components/Product.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../Css/Product.css';
+
+function Product({ id, title, image, price, rating }) {
+    return (
+        <div className="product">
+            <div className="product_info">
+                <p>{title}</p>
+                <p className="product_price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product_rating">
+                    {
+                        Array(rating).fill().map((_) => (
+                            <p><img height="21" width="21" src="https://img.icons8.com/emoji/48/000000/star-emoji.png"/></p>
+                        ))
+                    }
+                </div>
+            </div>
+            <img src={image} alt=""></img>
+            <button>Add to Cart</button>
+        </div>
+    )
+}
+
+export default Product
